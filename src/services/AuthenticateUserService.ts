8c472a464e97fc27b298b7d17f997cec3a8790ed
joaomcode <joaomarcelo.co@hotmail.com>
2020-04-26 09:37:17 -0300

    Autenticação - 02 e 03 - Validando credenciais e Gerando token JWT

---
+++ src/services/AuthenticateUserService.ts
@@ -0,0 +1,41 @@
+import { getRepository } from 'typeorm';
+import { sign } from 'jsonwebtoken';
+import { compare } from 'bcryptjs';
+import User from '../models/User';
+
+interface Request {
+  email: string;
+  password: string;
+}
+
+interface Response {
+  user: User;
+  token: string;
+}
+
+class AuthenticateUserService {
+  public async execute({ email, password }: Request): Promise<Response> {
+    const userRepository = getRepository(User);
+
+    const user = await userRepository.findOne({ where: { email } });
+
+    if (!user) {
+      throw new Error('Incorrect email/password combination.');
+    }
+
+    const passwordMatched = await compare(password, user.password);
+
+    if (!passwordMatched) {
+      throw new Error('Incorrect email/password combination.');
+    }
+
+    const token = sign({}, 'b34890b79ed48329de6a76dd8e66adad', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    return { user, token };
+  }
+}
+
+export default AuthenticateUserService;
