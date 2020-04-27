52d62336b3b3954e941743ee666edbe129f76766
joaomcode <joaomarcelo.co@hotmail.com>
2020-04-27 08:52:53 -0300

    Tratando exceções - 01 - Criando classe de erro

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,8 +1,11 @@
 import { getRepository } from 'typeorm';
 import { sign } from 'jsonwebtoken';
 import { compare } from 'bcryptjs';
-import User from '../models/User';
 import authConfig from '../config/auth';
+
+import AppError from '../errors/AppError';
+
+import User from '../models/User';
 
 interface Request {
   email: string;
@@ -21,13 +24,13 @@ class AuthenticateUserService {
     const user = await userRepository.findOne({ where: { email } });
 
     if (!user) {
-      throw new Error('Incorrect email/password combination.');
+      throw new AppError('Incorrect email/password combination.', 401);
     }
 
     const passwordMatched = await compare(password, user.password);
 
     if (!passwordMatched) {
-      throw new Error('Incorrect email/password combination.');
+      throw new AppError('Incorrect email/password combination.', 401);
     }
 
     const { secret, expiresIn } = authConfig.jwt;
