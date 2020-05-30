b5f53255a5b200ef8a51c4744171f612eda97d4f
joaomcode <joaomarcelo.co@hotmail.com>
2020-05-30 10:41:38 -0300

    Perfil do Usuário - 11 - Rota e controller de perfil

--- src/modules/users/services/UpdateProfileService.spec.ts
+++ src/modules/users/services/UpdateProfileService.spec.ts
@@ -36,6 +36,16 @@ describe('UpdateProfile', () => {
     expect(updatedUser.email).toBe('johntrois@example.com');
   });
 
+  it('should not be able to update the profile from a non-existing user', async () => {
+    expect(
+      updateProfile.execute({
+        user_id: 'non-existing-user-id',
+        name: 'non-existing-user-name',
+        email: 'non-existin-user-e-mailg@email.com',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
   it('should not be able to change to another user email', async () => {
     await fakeUsersRepository.create({
       name: 'John Doe',
